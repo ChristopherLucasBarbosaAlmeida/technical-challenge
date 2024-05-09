2802294983f7373fd94285c356b6c83f35a6fafd
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-05-08 23:51:00 -0300

    feat: add post listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { AxiosResponse } from "axios";
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Post } from "./components";
+import { axiosInstance } from "./libs/axios";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 export function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response: AxiosResponse<Post[]> = await axiosInstance.get("posts");
+      setPosts(response.data);
+    })();
+  }, []);
+
   return (
-    <main className="w-full h-[100dvh] flex justify-center items-center">
-      <h1 className="text-xl font-bold">techinical challenge</h1>
+    <main className="w-full h-[100dvh] flex justify-center items-center bg-indigo-50">
+      <div className="w-1/2 flex flex-col gap-4 h-2/3 overflow-auto">
+        <header className="w-1/2 flex justify-between items-center p-4 rounded-md bg-indigo-900 fixed top-16">
+          <strong className="text-lg text-white">Posts</strong>
+          <button className="w-36 p-2 flex justify-center items-center gap-2 bg-fuchsia-600 text-white rounded-md font-semibold">
+            <Plus />
+            Add post
+          </button>
+        </header>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            {...post}
+          />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/components/Pots.tsx
@@ -0,0 +1,22 @@
+import { MessageCircle } from "lucide-react";
+import { indigo } from "tailwindcss/colors";
+
+type PostProps = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+export function Post({ title, body }: PostProps) {
+  return (
+    <div className="w-full flex justify-between gap-4 p-4 rounded-lg bg-white first-child:mt-12">
+      <div>
+        <strong className="text-indigo-900 text-xl">{title}</strong>
+        <p className="text-gray-400">{body}</p>
+      </div>
+      <span className="flex items-center gap-2 font-bold text-indigo-900">
+        <MessageCircle color={indigo["300"]} />4
+      </span>
+    </div>
+  );
+}
