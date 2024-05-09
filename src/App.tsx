cfda3595f624b70f140131a4bf6ae9ddb56c7952
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-05-08 22:57:21 -0300

    feat: initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="w-full h-[100dvh] flex justify-center items-center">
+      <h1 className="text-xl font-bold">techinical challenge</h1>
+    </main>
+  );
+}
